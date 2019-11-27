c43fcc034e7f6fc139f40442472f798104c6dfcc
Shane Rogers <swrogers@gmail.com>
2019-11-27 08:28:57 -0600

    NgModel Form

---
+++ forms/src/app/demo-form-ng-model/demo-form-ng-model.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-ng-model',
+  templateUrl: './demo-form-ng-model.component.html',
+  styleUrls: ['./demo-form-ng-model.component.css']
+})
+export class DemoFormNgModelComponent implements OnInit {
+  productName: string;
+
+  constructor() {
+    this.productName = "ng-book: The Complete Guide to Angular";
+   }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
+  }
+
+}
